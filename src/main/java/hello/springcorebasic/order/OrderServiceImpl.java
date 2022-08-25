a3c1bf8379ba6fd2194a33ff9f224b900fc66c79
xhaktmchl <xhaktmchl@naver.com>
2022-08-26 01:20:13 +0900

    feat: requiredargconstructor를 이용한 의존주입 적용

--- src/main/java/hello/springcorebasic/order/OrderServiceImpl.java
+++ src/main/java/hello/springcorebasic/order/OrderServiceImpl.java
@@ -1,31 +1,25 @@
 package hello.springcorebasic.order;
 
 import hello.springcorebasic.discount.DiscountPolicy;
-import hello.springcorebasic.discount.FixDiscountPolicy;
-import hello.springcorebasic.discount.RateDiscountPolicy;
 import hello.springcorebasic.member.Member;
 import hello.springcorebasic.member.MemberRepository;
-import hello.springcorebasic.member.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 // 주문 서비스 구현체
 @Component
+@RequiredArgsConstructor // final 붙은 필드에 생성자 자동 생성 및 주입
 public class OrderServiceImpl implements OrderService{
 
-//    private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
-
-    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 율 할인 정책 구현체
     private final DiscountPolicy discountPolicy;
 
     //생성자 주입
-    @Autowired // @Autowired 를 사용하면 생성자에서 여러 의존관계도 한번에 주입받을 수 있다
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired // @Autowired 를 사용하면 생성자에서 여러 의존관계도 한번에 주입받을 수 있다
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice){
