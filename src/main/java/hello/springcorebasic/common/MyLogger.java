f1bed7cbb27f2d2f088ecd294218776b6fb7d81c
xhaktmchl <xhaktmchl@naver.com>
2022-08-28 22:45:22 +0900

    feat: Http request 요청을 받아 request 스코프 빈에대한 로그 남기기

---
+++ src/main/java/hello/springcorebasic/common/MyLogger.java
@@ -0,0 +1,35 @@
+package hello.springcorebasic.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL){
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message){
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct// 빈이 생성되는 시점에 자동으로 @PostConstruct 초기화 메서드를 사용해서 uuid를 생성해서저장
+    public void init(){
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy //이 빈이 소멸되는 시점에 @PreDestroy 를 사용해서 종료 메시지를 남긴다
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
---
+++ src/main/java/hello/springcorebasic/web/LogDemoController.java
@@ -0,0 +1,31 @@
+package hello.springcorebasic.web;
+
+
+import hello.springcorebasic.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor // 생성자 의존주입 실행
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider; // 빈 생명주기 직접 조절하기 위해
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
+
+        myLogger.setRequestURL(requestURL);
+        myLogger.log("controller test");
+        logDemoService.logic("testUUId");
+        return "OK";
+    }
+}
